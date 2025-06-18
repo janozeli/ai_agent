4899348e3ea64b65961df78255e4496ad3ea40e4
Janozeli <lucasmjanozeli@gmail.com>
2025-06-18 09:24:39 -0300

    get_files

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from google import genai
 from google.genai import types
 from sys import argv
 
+
 def main():
     user_prompt = argv[1] if len(argv) > 1 else None
     if not user_prompt:
@@ -16,13 +17,10 @@ def main():
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)])
-    ]
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages
+        model="gemini-2.0-flash-001", contents=messages
     )
 
     print(response.text)
@@ -32,6 +30,7 @@ def main():
         print(f"User prompt: {user_prompt}")
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
