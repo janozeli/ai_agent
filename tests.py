99ae65e5114f65a3b39c28be54c90db7e7f46aff
Janozeli <lucasmjanozeli@gmail.com>
2025-06-23 12:19:04 -0300

    calling function

--- tests.py
+++ tests.py
@@ -1,7 +1,6 @@
-import unittest
-from functions.get_files_info import get_files_info
+from functions.run_python import run_python_file
 
-print(get_files_info("calculator", "."))
-print(get_files_info("calculator", "pkg"))
-print(get_files_info("calculator", "/bin"))
-print(get_files_info("calculator", "../"))
+print(run_python_file("calculator", "main.py"))
+print(run_python_file("calculator", "tests.py"))
+print(run_python_file("calculator", "../main.py"))
+print(run_python_file("calculator", "nonexistent.py"))
